555bf41af17fb038ee82d6606cccc8e710cf7566
rnjsxordbs93 <rnjsxordbs93@gmail.com>
2025-04-24 12:06:44 +0900

    printf();를 이용한 간단한 사칙연산

---
+++ source/2025-03-07_printf_2/printf_2.c
@@ -0,0 +1,14 @@
+//사칙연산
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("10 + 3 = %d\n", 10 + 3);
+	printf("10 - 3 = %d\n", 10 - 3);
+	printf("10 x 3 = %d\n", 10 * 3);
+	printf("10 ÷ 3 = %.1f\n", 10. / 3.);
+	printf("10 ÷ 3 = (나머지) %d\n", 10 % 3);
+
+	return 0;
+}
